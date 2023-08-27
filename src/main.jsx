98845cf6b5d2cd3306cfde58ddb5fcf497415e1f
Ivan Papko <ivanpapko@coherentsolutions.com>
2023-08-27 17:40:45 +0200

    URL Params in Loaders

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import {
 } from "react-router-dom";
 import './index.css'
 import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
-import Contact from "./routes/contact";
+import Contact, {loader as contactLoader} from "./routes/contact";
 
 import ErrorPage from "./error-page";
 
@@ -21,6 +21,7 @@ const router = createBrowserRouter([
             {
                 path: "contacts/:contactId",
                 element: <Contact />,
+                loader: contactLoader
             },
         ],
     },
