83e86e7fea2b8b3fd983a14f66df2a8b86314956
Ivan Papko <ivanpapko@coherentsolutions.com>
2023-08-29 23:12:15 +0200

    Synchronizing URLs to Form State

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,11 +1,12 @@
 import {Outlet, NavLink, Link, useLoaderData, Form, redirect, useNavigation} from "react-router-dom";
+import { useEffect } from "react";
 import {getContacts, createContact} from "../contacts";
 
 export async function loader({ request }) {
     const url = new URL(request.url);
     const q = url.searchParams.get("q");
     const contacts = await getContacts(q);
-    return { contacts };
+    return { contacts, q };
 }
 
 export async function action() {
@@ -14,8 +15,11 @@ export async function action() {
 }
 
 export default function Root() {
-    const {contacts} = useLoaderData();
+    const {contacts, q} = useLoaderData();
     const navigation = useNavigation();
+    useEffect(() => {
+        document.getElementById("q").value = q;
+    }, [q]);
     return (
         <>
             <div id="sidebar">
@@ -28,6 +32,7 @@ export default function Root() {
                             placeholder="Search"
                             type="search"
                             name="q"
+                            defaultValue={q}
                         />
                         <div
                             id="search-spinner"
